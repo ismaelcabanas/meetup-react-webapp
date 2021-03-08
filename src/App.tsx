9a938b51926acd59c3df6af27c31d701a26a82aa
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-03-08 22:38:59 +0100

    First menu version with Router without definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {Nav, Container} from 'react-bootstrap';
 
 function App() {
   return (
     <Container fluid>
-      <Nav defaultActiveKey="/home" className="flex-column">
-        <Nav.Link href="/home">Home</Nav.Link>
-        <Nav.Link href="/my-meeting-groups">My Meeting Groups</Nav.Link>
-        <Nav.Link href="/my-meetings">My Meetings</Nav.Link>
-        <Nav.Link href="/meeting-groups">Explore Meeting Groups</Nav.Link>
-        <Nav.Link href="/administration/meetingGroupProposals">Administration</Nav.Link>
-        <Nav.Link href="/subscription">Subscription</Nav.Link>
-        <Nav.Link href="/register">Registration</Nav.Link>
-        <Nav.Link href="/emails">Emails</Nav.Link>
-      </Nav>
+      <Router>
+        <Nav defaultActiveKey="/home" className="flex-column">
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/my-meeting-groups">My Meeting Groups</Nav.Link>
+          <Nav.Link href="/my-meetings">My Meetings</Nav.Link>
+          <Nav.Link href="/meeting-groups">Explore Meeting Groups</Nav.Link>
+          <Nav.Link href="/administration/meetingGroupProposals">Administration</Nav.Link>
+          <Nav.Link href="/subscription">Subscription</Nav.Link>
+          <Nav.Link href="/register">Registration</Nav.Link>
+          <Nav.Link href="/emails">Emails</Nav.Link>
+        </Nav>
+      </Router>  
     </Container>
   );
 }
