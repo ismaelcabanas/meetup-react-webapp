359251644ff09bcf92be12969dd1c6adc96b5773
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-03-08 22:13:25 +0100

    Add simple menu options

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
+import {Nav, Container} from 'react-bootstrap';
 
 function App() {
   return (
-    <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">
-          Welcome To React-Bootstrap TypeScript Example
-        </h1>
-      </Jumbotron>
+    <Container fluid>
+      <Nav defaultActiveKey="/home" className="flex-column">
+        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link href="/my-meeting-groups">My Meeting Groups</Nav.Link>
+        <Nav.Link href="/my-meetings">My Meetings</Nav.Link>
+        <Nav.Link href="/meeting-groups">Explore Meeting Groups</Nav.Link>
+        <Nav.Link href="/administration/meetingGroupProposals">Administration</Nav.Link>
+        <Nav.Link href="/subscription">Subscription</Nav.Link>
+        <Nav.Link href="/register">Registration</Nav.Link>
+        <Nav.Link href="/emails">Emails</Nav.Link>
+      </Nav>
     </Container>
   );
 }
