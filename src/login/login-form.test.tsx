7b094e6d7403fbb76b2c79f3b845a837cb4962e1
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-03-09 21:46:15 +0100

    Tests for render login form page

---
+++ src/login/login-form.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { LoginForm } from './login-form';
+
+describe('Login render page', () => {
+    it('renders the login page', () => {
+        const {getByText} = render(<LoginForm />);
+        expect(getByText(/login/i)).toBeInTheDocument();
+    });
+    it('renders a submit button', () => {
+        const {getByText} = render(<LoginForm />);
+        expect(getByText("Log in")).toBeInTheDocument();
+    });
+    it('render 2 input components', () => {
+        const {getByLabelText} = render(<LoginForm />);
+        expect(getByLabelText(/email/i)).toBeInTheDocument();
+        expect(getByLabelText(/password/i)).toBeInTheDocument();
+    });
+  });
