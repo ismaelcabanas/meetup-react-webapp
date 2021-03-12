d03f4608762e5de01d453a9ea7b1ec24ae7ed958
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-03-12 23:11:01 +0100

    Refactor login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import {BrowserRouter as Router} from "react-router-dom";
-import {Nav, Container} from 'react-bootstrap';
-import { LoginControl } from './login/login-control';
+import LoginForm from './login/login-form'
 
 function App() {
   return (
-    <Router>
-      <Container fluid>
-            <LoginControl />
-            <Nav defaultActiveKey="/" className="flex-column">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/my-meeting-groups">My Meeting Groups</Nav.Link>
-              <Nav.Link href="/my-meetings">My Meetings</Nav.Link>
-              <Nav.Link href="/meeting-groups">Explore Meeting Groups</Nav.Link>
-              <Nav.Link href="/administration/meetingGroupProposals">Administration</Nav.Link>
-              <Nav.Link href="/subscription">Subscription</Nav.Link>
-              <Nav.Link href="/register">Registration</Nav.Link>
-              <Nav.Link href="/emails">Emails</Nav.Link>
-            </Nav>
-        </Container>
-    </Router>
+    <div className="App">
+      <LoginForm />
+    </div>
     
   );
 }
