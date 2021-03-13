fc2ff1d6fcf3288e33540283af487578e38987f0
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-03-13 09:55:14 +0100

    Design sign in form with material design

--- src/login/login-form.tsx
+++ src/login/login-form.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import { Grid, Paper, Avatar, TextField, Button, FormControlLabel, Checkbox, Typography, Link } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { green } from '@material-ui/core/colors';
 
 export function LoginForm() {
+
+    const paperStyle = {padding: 20, height:'70vh', width:280, margin:"20px auto"};
+    const avatarStyle = {backgroundColor: '#1bbd7e'};
+    const btnStyle = {margin: "8px 0"};
+
     return (
-        <div>
-            <h1>Sign In</h1>
-        </div>
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid 
+                    container
+                    justify="center">
+                    <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>                    
+                </Grid>
+                <Grid
+                    container
+                    justify="center">
+                    <h2>Sign In</h2>
+                </Grid>    
+                <TextField label="Username" placeholder="Enter username" fullWidth required/>
+                <TextField label="Password" placeholder="Enter password" type="password" fullWidth required/>
+                <FormControlLabel
+                    control={
+                        <Checkbox color="primary" />        
+                    }
+                    label="Remember me"
+                />
+                <Button type="submit" color="primary" variant="contained" style={btnStyle} fullWidth>Sign in</Button>
+                <Typography>
+                    <Link href="#">Forgot password?</Link>
+                </Typography>
+                <Typography> Don't you have an account? 
+                    <Link href="#"> Sign up </Link>
+                </Typography>
+            </Paper>        
+        </Grid>
     )
 }
 
