2107d24f24ab5f80ef48500eb6c43f33594f9d80
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-04-06 07:46:26 +0200

    Add semantic ui

--- src/user/component/signin/SignInForm.tsx
+++ src/user/component/signin/SignInForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { useForm } from "react-hook-form";
+import { Button, Form, Header, Grid } from 'semantic-ui-react';
 import SignInUseCase from "../../application/signin/SignInUseCase";
 
 type LoginData = {
@@ -31,42 +32,51 @@ export function SignInForm(props: SignInFormProps) {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <div>
-                        <label htmlFor="username">Username: </label>                        
-                        <input 
-                            id="username"
-                            name="username"
-                            type="text"
-                            ref={register({required: true})} 
-                            onChange={handleUsernameChange}  />
-                    </div>
-
-                    {errors.username && errors.username.type === "required" && (
-                    <div role="emailError" className="error">Username is required.</div>
-                    )}
-
-                    <div>
+        <Grid centered columns={2} style={{ height: '100vh' }} verticalAlign="middle">
+            <Grid.Column>
+                <Header as='h2' color='teal' textAlign='center'>
+                    Sign-in
+                </Header>
+                <Form size="small" onSubmit={handleSubmit(onSubmit)}>
+                    <Form.Field error={!!errors.username}>
+                        <label htmlFor="username">Username: </label>   
+                        <div className="ui left icon input">                 
+                            <input 
+                                id="username"
+                                name="username"
+                                type="text"
+                                placeholder="type your user name"
+                                ref={register({required: true})} 
+                                onChange={handleUsernameChange}  />                
+                            <i aria-hidden="true" className="user icon"></i>
+                        </div>
+                        {errors.username && errors.username.type === "required" && (
+                            <span role="alert" className="errorMessage">Username is required.</span>
+                        )}    
+                    </Form.Field>
+                    <Form.Field error={!!errors.password}>
                         <label htmlFor="password">Password: </label>
-                        <input 
-                            id="password"
-                            name="password"
-                            type="password" 
-                            autoComplete="password"
-                            ref={register({required: true})} 
-                            onChange={handlePasswordChange}  />
-                    </div>
-                    
-                    {errors.password && errors.password.type === "required" && (
-                    <div role="passwordError" className="error">Password is required.</div>
-                    )}
-
-                    <div><input name="sigin" type="submit" value="Sign in" /></div>
-                </div>
-            </form>            
-        </div>
+                        <div className="ui left icon input">   
+                            <input 
+                                id="password"
+                                name="password"
+                                type="password" 
+                                autoComplete="password"
+                                placeholder="type your password"
+                                ref={register({required: true})} 
+                                onChange={handlePasswordChange}  />                
+                            <i aria-hidden="true" className="lock icon"></i>    
+                        </div>
+                        {errors.password && errors.password.type === "required" && (
+                            <span role="alert" className="errorMessage">Password is required.</span>
+                        )}
+                    </Form.Field>
+                    <Form.Field>
+                        <Button primary type='submit' name="signin">Sign in</Button>        
+                    </Form.Field>            
+                </Form>
+            </Grid.Column>
+        </Grid>
     )
 }
 
